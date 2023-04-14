0e93b99283c5cd38cf7e98f327ad6b5ff08aa9da
German Rangel <gnrg_74@hotmail.com>
2023-04-14 14:04:19 -0600

    add new background and change primary color

--- portafolio/tailwind.config.js
+++ portafolio/tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#050816",
+        primary: "#000000",
         secondary: "#aaa6c3",
         tertiary: "#151030",
         "black-100": "#100d25",
@@ -20,7 +20,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
+        "hero-pattern": "url('/src/assets/bg.jpg')",
       },
     },
   },
